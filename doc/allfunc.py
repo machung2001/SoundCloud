726cf359dcfe6ce9bb49b303724b9e88026a404a
SilentCatD <36961907+SilentCatD@users.noreply.github.com>
2021-11-10 13:21:43 +0700

    Update allfunc.py

--- doc/allfunc.py
+++ doc/allfunc.py
@@ -3,22 +3,22 @@ from enum import Enum
 
 import requests
 import csv
-import pandas as pd
+
 #####################
 # testing variable, print this to see how many requests ha been made
 TOTAL_REQ = 0
 
 
-
-def savefile(file_name,json_data):
+def savefile(file_name, json_data):
     keys = set()
     for d in json_data:
         keys.update(d.keys())
-    keys=sorted(keys)
-    with open(file_name, 'a',encoding = 'utf-8') as output_file:
-        dict_writer = csv.DictWriter(output_file, restval="-", fieldnames=keys,delimiter = '\t')
+    keys = sorted(keys)
+    with open(file_name, 'a', encoding='utf-8') as output_file:
+        dict_writer = csv.DictWriter(output_file, restval="-", fieldnames=keys, delimiter='\t')
         dict_writer.writeheader()
         dict_writer.writerows(json_data)
+
 
 # save to some file to debug results
 def temp_save(json_data, file):
@@ -28,20 +28,21 @@ def temp_save(json_data, file):
 
 #########################################
 
-def request_url(url, max_req=20):
+def request_url(url, max_req=10):
     global TOTAL_REQ
     req = 0
     while True:
         response = requests.get(url)
         if not response.ok:
-            print(f"Failed {response.url}")
+            #print(f"Failed {response.url}")
             req += 1
             if req < max_req:
                 continue
             else:
-                raise ConnectionError("Check your internet or url again")
+                print(f'Aborted url: {response.url}')
+                return {}
         break
-    print(f"Hit {response.url}")
+    #print(f"Hit {response.url}")
     # Testing variable
     TOTAL_REQ += 1
     return response.json()
@@ -99,18 +100,21 @@ def get_query_item(q_type, query, client_id, result_limit):
     if sub_url:
         item_ids.extend(get_id_from_collection(sub_url, client_id, result_limit))
     for item_id in item_ids:
-        results.append(func(item_id, client_id))
+        item_info = func(item_id, client_id)
+        if item_info:
+            results.append(item_info)
     return results
 
 
 def extract_playlist_generals(playlist_id, client_id):
     url = f'https://api-v2.soundcloud.com/playlists/{playlist_id}?client_id={client_id}&limit=100&linked_partitioning=1'
     generals_data = request_url(url)
-    generals_data['user'] = generals_data['user']['id']
-    tracks_list = []
-    for track in generals_data['tracks']:
-        tracks_list.append(track['id'])
-    generals_data['tracks'] = list(set(tracks_list))
+    if generals_data:
+        generals_data['user'] = generals_data['user']['id']
+        tracks_list = []
+        for track in generals_data['tracks']:
+            tracks_list.append(track['id'])
+        generals_data['tracks'] = list(set(tracks_list))
     return generals_data
 
 
@@ -118,8 +122,9 @@ def playlist_info(playlist_id, client_id):
     reposters_url = f'https://api-v2.soundcloud.com/playlists/{playlist_id}/reposters?client_id={client_id}&limit=100&linked_partitioning=1'
     likers_url = f'https://api-v2.soundcloud.com/playlists/{playlist_id}/likers?client_id={client_id}&limit=100&linked_partitioning=1'
     generals_data = extract_playlist_generals(playlist_id, client_id)
-    generals_data['reposters'] = get_id_from_collection(reposters_url, client_id, 100)
-    generals_data['likers'] = get_id_from_collection(likers_url, client_id, 100)
+    if generals_data:
+        generals_data['reposters'] = get_id_from_collection(reposters_url, client_id, 100)
+        generals_data['likers'] = get_id_from_collection(likers_url, client_id, 100)
     return generals_data
 
 
@@ -128,7 +133,9 @@ def get_featured_tracks(client_id, result_limit=50):
     url = f'https://api-v2.soundcloud.com/featured_tracks/top/all-music?linked_partitioning=1&client_id={client_id}&limit=100'
     featured_tracks = set(get_id_from_collection(url, client_id, result_limit))
     for track in featured_tracks:
-        results.append(track_info(track, client_id))
+        ti = track_info(track, client_id)
+        if ti:
+            results.append(ti)
     return results
 
 
@@ -192,7 +199,9 @@ def get_charts(client_id):
             charts_tracks.extend(extract_charts_data(url, client_id))
     charts_tracks = set(charts_tracks)
     for track in charts_tracks:
-        results.append(track_info(track, client_id))
+        ti = track_info(track, client_id)
+        if ti:
+            results.append(ti)
     return results
 
 
@@ -201,6 +210,8 @@ def get_discover_id(client_id):
     playlists = []
     url = f'https://api-v2.soundcloud.com/mixed-selections?client_id={client_id}&limit=100&linked_partitioning=1'
     response = request_url(url)
+    if not response:
+        return tracks, playlists
     collections = response['collection']
     for collection in collections:
         if 'charts' in collection['urn']:
@@ -220,18 +231,22 @@ def get_discover(client_id):
 
     track_result = []
     playlist_result = []
-
     for track_id in tracks:
-        track_result.append(track_info(track_id, client_id))
+        ti = track_info(track_id, client_id)
+        if ti:
+            track_result.append(ti)
     for playlist_id in playlists:
-        playlist_result.append(playlist_info(playlist_id, client_id))
+        pi = playlist_info(playlist_id, client_id)
+        if pi:
+            playlist_result.append(pi)
     return track_result, playlist_result
 
 
 def extract_track_generals(track_id, client_id):
     url = f'https://api-v2.soundcloud.com/tracks/{track_id}?client_id={client_id}&linked_partitioning=1'
     generals_data = request_url(url)
-    generals_data['user'] = generals_data['user']['id']
+    if generals_data:
+        generals_data['user'] = generals_data['user']['id']
     return generals_data
 
 
@@ -244,16 +259,17 @@ def track_info(track_id, client_id):
     comments_url = f'https://api-v2.soundcloud.com/tracks/{track_id}/comments?threaded=0&filter_replies=0&client_id={client_id}&limit=100&linked_partitioning=1'
 
     generals_data = extract_track_generals(track_id, client_id)
-    generals_data['reposters'] = get_id_from_collection(reposters_url, client_id, 100)
-    generals_data['likers'] = get_id_from_collection(likers_url, client_id, 100)
-    generals_data['comments'] = get_id_from_collection(comments_url, client_id, 100, 'user')
-    generals_data['related_tracks'] = get_id_from_collection(related_tracks_url, client_id, 100)
-    generals_data['album'] = get_id_from_collection(album_url, client_id, 100)
-    generals_data['playlists'] = get_id_from_collection(playlists_url, client_id, 100)
+    if generals_data:
+        generals_data['reposters'] = get_id_from_collection(reposters_url, client_id, 100)
+        generals_data['likers'] = get_id_from_collection(likers_url, client_id, 100)
+        generals_data['comments'] = get_id_from_collection(comments_url, client_id, 100, 'user')
+        generals_data['related_tracks'] = get_id_from_collection(related_tracks_url, client_id, 100)
+        generals_data['album'] = get_id_from_collection(album_url, client_id, 100)
+        generals_data['playlists'] = get_id_from_collection(playlists_url, client_id, 100)
 
-    generals_data.pop('publisher_metadata')
-    generals_data.pop('media')
-    generals_data.pop('visuals')
+        generals_data.pop('publisher_metadata')
+        generals_data.pop('media')
+        generals_data.pop('visuals')
     return generals_data
 
 
@@ -272,40 +288,37 @@ def user_info(user_id, client_id):
     followers_url = f'https://api-v2.soundcloud.com/users/{user_id}/followers?client_id={client_id}&limit=100&linked_partitioning=1'
 
     generals_data = request_url(general_url)
-    generals_data['web_profile'] = request_url(web_profile_url)
-    generals_data['spotlight_tracks'] = get_id_from_collection(spotlight_url, client_id, 100)
-    generals_data['user_tracks'] = get_id_from_collection(user_tracks_url, client_id, 100)
-    generals_data['user_top_tracks'] = get_id_from_collection(user_top_tracks_url, client_id, 100)
-    generals_data['user_albums'] = get_id_from_collection(user_albums_url, client_id, 100)
-    generals_data['user_playlist_without_albums'] = get_id_from_collection(user_playlist_without_albums_url, client_id,
-                                                                           100)
-    generals_data['related_artist'] = get_id_from_collection(related_artist_url, client_id, 100)
-    generals_data['followings'] = get_id_from_collection(followings_url, client_id, 100)
-    generals_data['followers'] = get_id_from_collection(followers_url, client_id, 100)
-    generals_data['reposts'] = get_id_from_collection(reposts_url, client_id, 100, 'track')
-    generals_data['likes'] = get_id_from_collection(likes_url, client_id, 100, 'track')
+    web_profile = request_url(web_profile_url)
+    if generals_data and web_profile:
+        generals_data['web_profile'] = web_profile
+        generals_data['spotlight_tracks'] = get_id_from_collection(spotlight_url, client_id, 100)
+        generals_data['user_tracks'] = get_id_from_collection(user_tracks_url, client_id, 100)
+        generals_data['user_top_tracks'] = get_id_from_collection(user_top_tracks_url, client_id, 100)
+        generals_data['user_albums'] = get_id_from_collection(user_albums_url, client_id, 100)
+        generals_data['user_playlist_without_albums'] = get_id_from_collection(user_playlist_without_albums_url, client_id,
+                                                                               100)
+        generals_data['related_artist'] = get_id_from_collection(related_artist_url, client_id, 100)
+        generals_data['followings'] = get_id_from_collection(followings_url, client_id, 100)
+        generals_data['followers'] = get_id_from_collection(followers_url, client_id, 100)
+        generals_data['reposts'] = get_id_from_collection(reposts_url, client_id, 100, 'track')
+        generals_data['likes'] = get_id_from_collection(likes_url, client_id, 100, 'track')
 
-    generals_data.pop('creator_subscription')
-    generals_data.pop('creator_subscriptions')
-    generals_data.pop('visuals')
-    generals_data.pop('badges')
+        generals_data.pop('creator_subscription')
+        generals_data.pop('creator_subscriptions')
+        generals_data.pop('visuals')
+        generals_data.pop('badges')
 
-    web_profile = generals_data.pop('web_profile', None)
-    socials = []
-    if web_profile:
-        for social in web_profile:
-            socials.append(social['url'])
-    generals_data['socials'] = socials
+        web_profile = generals_data.pop('web_profile', None)
+        socials = []
+        if web_profile:
+            for social in web_profile:
+                socials.append(social['url'])
+        generals_data['socials'] = socials
 
     return generals_data
 
 
 def main():
     client_id = 'qgbUmYdRbdAL2R1aLbVCgwzC7mvh8VKv'
-    query = 'imagine dragon'
-    query_result_users = get_query_item(QueryType.USERS, query, client_id, 100)
-    query_result_tracks = get_query_item(QueryType.TRACKS, query, client_id, 100)
-    query_result_playlist = get_query_item(QueryType.PLAYLISTS, query, client_id, 100)
-
 
 main()
